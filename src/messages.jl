e7a3474016335abfa269b60a847a4dec8860a98a
Jake W. Ireland <jakewilliami@icloud.com>
2020-10-07 11:26:11 +1300

    Began work on alphabet

---
+++ src/messages.jl
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+    #=
+    exec julia --project="$(realpath $(dirname $0))" --color=yes --startup-file=no -e "include(popfirst!(ARGS))" \
+    "${BASH_SOURCE[0]}" "$@"
+    =#
+
+struct Alphabet
+    Σ::Union{AbstractArray, AbstractString}
+    
+    function Alphabet(Σ::AbstractArray)
+        try
+            Σ = parse.(Int, Σ)
+        catch
+        end
+        
+        new(Σ)
+    end # end constructor function
+    
+    function Alphabet(Σ::AbstractString)
+        Σ = collect(Σ)
+        
+        try
+            Σ = parse.(Int, Σ)
+        catch
+            Σ = string.(Σ)
+        end
+        
+        new(Σ)
+    end # end constructor function
+end # end struct
+
+struct Messages
+    ℳ::AbstractArray
+end
