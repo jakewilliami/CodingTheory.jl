73a5c5797978408f20854efc31452c548528eec3
Jake W. Ireland <jakewilliami@icloud.com>
2020-10-22 21:15:50 +1300

    Added function to search through all codewords

--- src/CodingTheory.jl
+++ src/CodingTheory.jl
@@ -7,7 +7,8 @@
 module CodingTheory
 
 export Alphabet, Messages, rate, sphere_covering_bound, sphere_packing_bound,
-        construct_ham_matrix, isperfect, ishammingperfect, isgolayperfect
+        construct_ham_matrix, isperfect, ishammingperfect, isgolayperfect,
+        get_codewords, get_all_words
 export list_polys, multiplication_table, list_span, islinear, isirreducible
 export hamming_distance, hamming_ball, code_distance, t_error_detecting,
         t_error_correcting, find_error_detection_max, find_error_correction_max
