cd6152bf4e5197c7d0e7f39fe07e2c134c8d228b
Jake W. Ireland <jakewilliami@icloud.com>
2021-01-12 19:04:17 +1300

    Added deploy docs statement

--- docs/make.jl
+++ docs/make.jl
@@ -14,16 +14,16 @@ Documenter.makedocs(
     ]
 )
 
-# deploydocs(;
-#     repo  =  "github.com/jakewilliami/CodingTheory.jl.git",
-# )
-
-deploydocs(
-    target = "build",
-    repo   = "github.com/jakewilliami/CodingTheory.jl.git",
-    branch = "gh-pages",
-    devbranch = "master",
-    devurl = "dev",
-    versions = ["stable" => "v^", "v#.#.#", "dev" => "dev"],
-    push_preview    = false
+deploydocs(;
+    repo  =  "github.com/jakewilliami/CodingTheory.jl.git",
 )
+
+# deploydocs(
+#     target = "build",
+#     repo   = "github.com/jakewilliami/CodingTheory.jl.git",
+#     branch = "gh-pages",
+#     devbranch = "master",
+#     devurl = "dev",
+#     versions = ["stable" => "v^", "v#.#.#", "dev" => "dev"],
+#     push_preview    = false
+# )
