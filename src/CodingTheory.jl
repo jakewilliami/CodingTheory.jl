915e99fbeecbaa50dc840745163078289d38daf7
Jake W. Ireland <jakewilliami@icloud.com>
2021-01-12 19:18:51 +1300

    Minor formatting changes to exports

--- src/CodingTheory.jl
+++ src/CodingTheory.jl
@@ -29,16 +29,17 @@ export rref, rref!
 # Messages, Distance, and Primes
 export rate, sphere_covering_bound, sphere_packing_bound, hamming_bound,
         singleton_bound, gilbert_varshamov_bound, elias_bassalygo_bound,
-        plotkin_bound,
-        construct_ham_matrix, isperfect, ishammingperfect, isgolayperfect,
-        get_codewords_greedy, get_codewords_random, get_all_words,
-        get_codewords
+        plotkin_bound, construct_ham_matrix, isperfect, ishammingperfect,
+        isgolayperfect, get_codewords_greedy, get_codewords_random,
+        get_all_words, get_codewords
 export hamming_distance, hamming_ball, code_distance, t_error_detecting,
         t_error_correcting, find_error_detection_max, find_error_correction_max
 export isprimepower
 
 # Algebra
-export FinitePolynomial, list_polys, multiplication_table, list_span, islinear, isirreducible, normal_form!, normal_form, equivalent_code!, equivalent_code, generator!, generator, parity_check, syndrome, isincode
+export FinitePolynomial, list_polys, multiplication_table, list_span, islinear,
+        isirreducible, normal_form!, normal_form, equivalent_code!, equivalent_code,
+        generator!, generator, parity_check, syndrome, isincode
 
 # Levenshtein
 export levenshtein, levenshtein!
